51c4ae5a0f028aeaf61328d4f5f4e50b3d89cac7
Behzad Barzin <behzad_barzin@live.com>
2024-08-04 05:26:09 +0330

    Basic TS server with Express and Prisma

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response, json } from "express";
+
+// Make sure to import the env file first
+import "./utils/env";
+
+export async function createServer() {
+  const app: Express = express();
+
+  // Serve static files
+  app.use(express.static("public"));
+
+  // Parse JSON request bodies
+  app.use(json());
+
+  app.get("/", (req: Request, res: Response) => {
+    res.json({ data: "Hello World!" });
+  });
+
+  return app;
+}
