bc9f93edab5b0d53b50efe7a912faf034b946e37
Behzad Barzin <behzad_barzin@live.com>
2024-08-06 00:14:02 +0330

    Adds error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response, json } from 'express';
+import 'express-async-errors'; // Must be imported at the beg
 
 // Make sure to import the env file first
 import './utils/env';
+import { NotFoundError } from './errors/not-found.error';
+import { errorHandler } from './middlewares/error-handler.middleware';
+import { requestLogger } from './middlewares/request-logger.middleware';
 
 export async function createServer() {
   const app: Express = express();
@@ -12,9 +16,27 @@ export async function createServer() {
   // Parse JSON request bodies
   app.use(json());
 
+  // Setup request logger
+  app.use(requestLogger);
+
   app.get('/', (req: Request, res: Response) => {
-    res.json({ data: 'Hello World!' });
+    throw new NotFoundError();
   });
+
+  // ---------------------------------------------------------------------------
+  // Add custom routes here
+  // ---------------------------------------------------------------------------
+
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // Catch all unhandled routes and forward to error handler
+  app.all('*', (req, res) => {
+    throw new NotFoundError();
+  });
+
+  // Catch all errors and forward to error handler middleware
+  app.use(errorHandler);
+  // ---------------------------------------------------------------------------
 
   return app;
 }
---
+++ src/middlewares/request-logger.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { logger } from '../utils/logger';
+
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+  const { method, path: url } = req;
+  const logMessage = `${method}: ${url}`;
+
+  logger.verbose(logMessage);
+  next();
+};
